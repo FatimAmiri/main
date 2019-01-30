d6e4e4c6ca186cf6567a0ece22bc96d6dd902347
COMPAS doc bot (from Travis CI) <deploy@travis-ci.org>
2019-01-30 14:48:19 +0000

    Deploy compas-dev/main to github.com/compas-dev/main.git:master

---
+++ api/generated/compas-datastructures-trimesh_vertexarea_matrix-1.py
@@ -0,0 +1,22 @@
+import compas
+
+from compas.datastructures import Mesh
+from compas.datastructures import mesh_quads_to_triangles
+from compas.datastructures import trimesh_vertexarea_matrix
+from compas.plotters import MeshPlotter
+
+mesh = Mesh.from_obj(compas.get('faces.obj'))
+
+mesh_quads_to_triangles(mesh)
+A = trimesh_vertexarea_matrix(mesh)
+area = A.diagonal().tolist()
+
+plotter = MeshPlotter(mesh, tight=True)
+
+plotter.draw_vertices(
+    text={key: "{:.1f}".format(area[i]) for i, key in enumerate(mesh.vertices())},
+    radius=0.2
+)
+plotter.draw_edges()
+plotter.draw_faces()
+plotter.show()
