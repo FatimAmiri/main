bc091d38145af1b1f8faa943e0dabb51ec357a2f
COMPAS doc bot (from Travis CI) <deploy@travis-ci.org>
2019-07-19 08:56:44 +0000

    Deploy compas-dev/main to github.com/compas-dev/main.git:master

---
+++ tutorials/nbtest-1.py
@@ -0,0 +1,21 @@
+import compas
+from compas.datastructures import Mesh
+from compas_plotters import MeshPlotter
+
+mesh = Mesh.from_obj(compas.get('faces.obj'))
+
+plotter = MeshPlotter(mesh)
+
+plotter.draw_vertices(
+    facecolor={key: '#ff0000' for key in mesh.vertices_on_boundary()},
+    radius={key: 0.3 for key in mesh.vertices_on_boundary()},
+    text={key: str(key) for key in mesh.vertices_on_boundary()})
+
+plotter.draw_edges(
+    color={key: '#00ff00' for key in mesh.edges_on_boundary()},
+    width={key: 3 for key in mesh.edges_on_boundary()})
+
+plotter.draw_faces(
+    text={key: str(key) for key in mesh.faces_on_boundary()})
+
+plotter.show()
